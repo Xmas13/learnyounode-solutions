be4a246bc2b8a0d29f73966e6c95839ba540fa4c
Noel Miller <noel@asolan.io>
2016-04-06 12:46:04 -0500

    Added comments to ex6 to ensure I fully grasp the content

--- ex6/app.js
+++ ex6/app.js
@@ -1,8 +1,17 @@
+// Require module I created to run function
+var filterbyext = require('./filterbyext.js');
+// Set variables to pass into the function required
 var dir = process.argv[2];
 var ext = process.argv[3];
-var filterbyext = require('./filterbyext.js');
 
-filterbyext.readDirectory(dir, ext, function(err, files) {
+/* Use the readDirectory function from the 
+filterbyext module to take 3 arguments as defined
+by the function in the module. dir, ext,
+and callback. We use printContents to process
+the data we get back from readDirectory to print
+out the results. This takes 2 arguments, err and files.
+*/
+filterbyext.readDirectory(dir, ext, function printContents(err, files) {
 	for (i = 0; i < files.length; i++) {
 		console.log(files[i]);
 	}
--- ex6/filterbyext.js
+++ ex6/filterbyext.js
@@ -1,8 +1,19 @@
 
+// Require modules that make the module work
 var fs = require('fs');
 var path = require('path');
-var exports = module.exports = {};
-exports.readDirectory = function (dir, ext, callback) {
+
+/* We use the module.exports function to export
+a set (or in our case just one) modules by name.
+It goes name, then the anonymous function.*/
+module.exports = {
+/* Just like our previous exercise our function
+takes in the first 2 arguments which are dir and
+ext. We add to it the argument callback. This will
+return all the information that will be used to 
+pass into another function. (for printing to
+console as an example). */
+readDirectory : function (dir, ext, callback) {
 	fs.readdir(dir, function doneReading (err, directory) {
 		if (err)
 			return callback(err)
@@ -13,8 +24,12 @@ exports.readDirectory = function (dir, ext, callback) {
 				arr.push(directory[i]);
 			}
 		}
+		/* we need to ensure we use null as the first argument
+		as we checked for errors before getting to our callback.
+		Second argument is what is returned for the callback. */
 		callback(null, arr);
 	})
 	
+	}
 }
 
